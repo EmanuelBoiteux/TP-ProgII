b0c96eac6c7f217f90664a14cc1350e667bd3884
Alumno <emanuelbboiteux@gmail.com>
2023-11-28 11:14:04 -0300

    avances

--- main.c
+++ main.c
@@ -4,65 +4,68 @@
 #include <assert.h>
 #include <ctype.h>
 
-FILE* limpiaTexto(FILE* archivo){
+void limpiaTexto(FILE* archivo){
     FILE* salida;
-    salida = fopen("Textos/Entradas/Fito_Paez.txt", "w");
+    salida = fopen("Entradas/Fito_Paez.txt", "w");
 
     char linea[255];
-
+    int punto = 1;
     for (int i = 0; fgets(linea, 255, archivo) != NULL; i++){
         for (int j = 0; j < strlen(linea) - 1; j++){
             if (isalpha(linea[j]) || isspace(linea[j])){
-                fputc(linea[j], salida);
+                if (isupper(linea[j])){
+                    if (punto == 0){
+                        fputc(' ', salida);
+                        fputc(tolower(linea[j]), salida);
+                    }
+                    else {
+                        fputc(tolower(linea[j]), salida);
+                        punto = 0;
+                    }
+                }
+                else {
+                    fputc(linea[j], salida);
+                    punto = 0;
+                }
             }
             else if (linea[j] == '.'){
-                fputs(".\n", salida);
-            }
-            else if (linea[j] == '\n'){
-                fputc(' ', salida);
+                punto = 1;
+                fputc('\n', salida);
             }
         }
     }
-
-    return salida;
+    fclose(salida);
 }
 
 
 char* creaRuta(char persona[]){
     char* ruta = malloc(100);
-    strcpy(ruta, "Textos/");
-    strcat(strcat(strcat(strcat(ruta, persona), "/"), persona), ".txt");
+    strcat(strcpy(ruta, "Textos/"), persona);
     return ruta;
 }
 
-void creaRutaTest(){
-    char *ruta_test = creaRuta("Cerati");
-    assert(strcmp(ruta_test, "Textos/Cerati/Cerati.txt") == 0);
-    strcpy(ruta_test, creaRuta("Leo_Messi"));
-    assert(strcmp(ruta_test, "Textos/Leo_Messi/Leo_Messi.txt") == 0);
-    free(ruta_test);
-}
+
 
 int main(int argc, char *argv[]){
     FILE *archivo;
 
     // TEST
-    creaRutaTest();
+    //creaRutaTest();
     // END TEST
 
     char* direc = creaRuta(argv[1]);
     archivo = fopen(direc, "r");
 
+    printf("%s\n", direc);
+    char ruta[30];
+    strcat(strcat(strcpy(ruta, "cd "), direc), " && ls");
+    printf("%s\n", ruta);
+
+    int r = system(ruta);
+    printf("%d\n", r);
+
     limpiaTexto(archivo);
-    /*
-    char linea[255];
-    fgets(linea, 255, archivo);
-    printf("%s\n", linea);
-    */
-
-    //int r = system("cd Textos && ls");
-    //printf("%d\n", r);
-
+    
     free(direc);
     fclose(archivo);
     return 0;
---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <ctype.h>
+
+void creaRutaTest(){
+    char *ruta_test = creaRuta("Cerati");
+    assert(strcmp(ruta_test, "Textos/Cerati/Cerati.txt") == 0);
+    strcpy(ruta_test, creaRuta("Leo_Messi"));
+    assert(strcmp(ruta_test, "Textos/Leo_Messi/Leo_Messi.txt") == 0);
+    free(ruta_test);
+}
