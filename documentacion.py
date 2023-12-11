61dd5cf5e3ad6588030abbbcb6d7eb70b7ace628
Emanuel Boiteux <emanuelbboiteux@gmail.com>
2023-12-11 12:51:10 -0300

    detalles finales, trabajo terminado

--- documentacion.py
+++ documentacion.py
@@ -25,13 +25,13 @@ def palabrasAntePos(palabra: str, listaPalabras: list, n: int) -> dict:
     de la palabra pasada, siendo i su indice.
     - si n = -1 devuelve un diccionario cuyas claves son las palabras de la lista que se encuentran en una posicion i + 1 respecto a las ocurrencias en la lista
     de la palabra pasada, siendo i su indice.
-    En cualquier caso los valores del diccionario son lasa ocurrencias de cada palabra. Si la palabra pasada no tiene palabras anteriores o posteriores se devuelve {}.
+    En cualquier caso, los valores del diccionario son las ocurrencias de cada palabra. Si la palabra pasada no tiene palabras anteriores o posteriores se devuelve {}.
     """
     pass
 
 def mayorFrecuencia(palabrasList: list) -> str:
     """
-    mayorFrecuencia recibe un a lista de palabras que contiene guiones, elimina estos guiones y devuelve la palabra que mas veces aparecio en la lista 
+    mayorFrecuencia recibe un a lista de palabras que contiene guiones, elimina estos guiones y devuelve la palabra que mas veces aparecio en la lista.
     """
     pass
 
@@ -43,11 +43,11 @@ def completaFrase(nombre: str, diccionarioPalabras: dict, palabraRepetida: str):
     """
     pass
 
-def remplazaGuion(anterior: str, siguiente: str, palabrasDict: dict[dict], linea: str, masFrecuente: str, archivoSalida):
+def remplazaGuion(anterior: str, siguiente: str, palabrasDict: dict, linea: str, masFrecuente: str, archivoSalida):
     """
     Esta funcion recibe una palabra anterior y una palabra siguiente a un guion, un diccionario de palabras, la linea donde se encuentra el guion,
-    la palabra mas frecuente del mismo y un archivo. Se encarga de escribir el archivo pasado como argumento
-    un archivo de texto donde los guiones que estan en la linea son remplazados por palabras de acuerdo al siguiente criterio: 
+    la palabra mas frecuente del archivo de texto y ese archivo. Se encarga de remplazar en el archivo pasado como argumento
+    las posiciones de los guiones por palabras de acuerdo al siguiente criterio: 
     - si la palabra anterior al guion se encuentra en el diccionario pasado y en el texto hay palabras que le siguen, entonces se coloca en la posicion del la palabra
     que mas veces siguio despues de esta palabra
     - si la palabra siguiente al guion se encuentra en el diccionario pasado y en el texto hay palabras que le anteceden, entonces se coloca en la posicion del guion la palabras
@@ -56,7 +56,7 @@ def remplazaGuion(anterior: str, siguiente: str, palabrasDict: dict[dict], linea
     """
     pass
 
-def masRepetida(dictPalabras: dict[str]) -> str:
+def masRepetida(dictPalabras: dict) -> str:
     """
     Esta funcion recibe un diccionario cuyas claves son palabras y los valores de cada palabra son enteros que corresponden a su ocurrencia.
     masRepedida devuelve la palabra del diccionario con mayor ocurrencia.
--- main.h
+++ main.h
@@ -6,15 +6,15 @@
 
 void getArchivos(char nombre[]);
 // getArchivos recibe el nombre de una persona y crea un archivo de texto que en cada linea tiene el nombre 
-// de los archivos que se encuentran en la carpeta Textos/<nombre recibido>. Utiliza la funcion system.
+// de uno de los archivos que se encuentran en la carpeta Textos/<nombre recibido>. Utiliza la funcion system.
 
 void getNombre(char persona[]);
-// Esta funcion recibe el nombre de una persona y lee el archivo creado por la funcion getArchivos() linea por linea.
+// Esta funcion recibe el nombre de una persona y lee el archivo creado por la funcion getArchivos linea por linea.
 // A cada linea leida, le elimina el caracter '\0'. Luego, llama a la funcion limpiaTexto.
 
 void limpiaTexto(FILE* archivo, char persona[]);
 // Esta funcion recibe un puntero a un archivo de texto y el nombre de una persona, y luego crea un nuevo archivo de texto
-// donde solo hay caracteres correspondinetes a letras minusculas y saltos de linea. Se eliminan los caracteres especiales, simbolos,
+// donde solo hay caracteres correspondinetes a letras minusculas, saltos de linea y espacios. Se eliminan los caracteres especiales, simbolos,
 // y las letras mayusculas son pasadas a minusculas. Este archivo es creado en la carpeta Entradas con el nombre de la persona recibida.
 
 void inicializePython(char nombre[]);
